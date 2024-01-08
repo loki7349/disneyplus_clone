17212cf694215473d77aff8ae1bafff8b0f46f26
loki7349 <lokeshargon19l@gmail.com>
2024-01-08 23:20:50 +0530

    added the Sign up button with description

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,22 @@
 import styled from "styled-components"
 
+
 import React from 'react'
 
 const Login = () => {
   return(
     <Container>
         <Content>
+            <CTA>
+                <CTALogoOne src="/images/cta-logo-one.svg" alt='' />
+                <SignUp>GET ALL THERE</SignUp>
+                <Description>
+                    Get Premier Access to Raya and the Last Dragon for an additional fee
+                    with a Disney+ subscription. As of 08/01/2024, the price of Disney+
+                    and The Disney Bundle will increase by $1.
+                </Description>
+                <CTALogoTwo src="/images/cta-logo-two.png" alt='' />
+            </CTA>
             <BgImage />
         </Content>
     </Container>
@@ -43,5 +54,52 @@ top: 0;
 right:0;
 left: 0;
 z-index: -1;
+`
+
+const CTA = styled.div`
+    max-width: 650px;
+    display: flex;
+    flex-direction: column;
+    width: 100%
+`
+
+const CTALogoOne = styled.img`
+    margin-bottom: 12px;
+    max-width: 600px;
+    min-height:1px;
+    display: block;
+    width: 100%;
+`
+
+const SignUp = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+
+    &:hover {
+        background-color: #0483ee
+    }
+`
+
+const Description = styled.p`
+    color: hsla(0, 0%, 95.3%, 1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-spacing: 1.5;
+    letter-spacing: 1.5px;
+`
+const CTALogoTwo = styled.img`
+    max-width: 600px;
+    margin-bottom: 20px;
+    display: inline-block;
+    verticle-align: bottom;
+    width: 100%;
 `
 export default Login
