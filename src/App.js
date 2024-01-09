c65d757320a0cc0488e1e5709d3b2c55384fcea9
loki7349 <lokeshargon19l@gmail.com>
2024-01-09 23:25:12 +0530

    Added the Nav-bar section to the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login'
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Routes>
-        <Route exact path='/' element={<Login/>}/>
-      </Routes>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Login/>}/>
+        </Routes>
       </Router>
       
     </div>
