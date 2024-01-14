70f9b4ddedebb2085647f8968b10cca40cab69cc
loki7349 <lokeshargon19l@gmail.com>
2024-01-14 23:29:34 +0530

    Added the sign in and redirect user

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { auth, provider } from '../firebase'
 import { useDispatch, useSelector } from 'react-redux'
@@ -10,6 +10,15 @@ function Header(){
     const history = useHistory();
     const username = useSelector(selectUserName)
     const userPhoto = useSelector(selectUserPhoto)
+
+    useEffect(() => {
+        auth.onAuthStateChanged(async (user) =>{
+            if(user){
+                setUser(user)
+                history.push('/home')
+            }
+        })
+    },[username]);
 
 
     const handleAuth = () => {
@@ -36,15 +45,9 @@ function Header(){
             <img src="/images/logo.svg" alt='Disney+' />
         </Logo>
         {
-            !username ? <Login onClick={handleAuth}>Login</Login>: 
-            <>
-            <NavMenu>
-                <a href='/home'>
-                    <img src='/images/home-icon.svg' alt='HOME' />
-                </a>
-            </NavMenu>
-            </>
-        }
+            !username ? (<Login onClick={handleAuth}>Login</Login>
+                ):(
+                    <>
         <NavMenu>
             <a href='/home'>
                 <img src='/images/home-icon.svg' alt='Home' />
@@ -72,9 +75,14 @@ function Header(){
             </a>
             
         </NavMenu>
-        <Login onClick={handleAuth}>
-            Login
-        </Login>
+        <SignOut>
+        <UserImg src={userPhoto} alt={username} />
+        <DropDown>
+            <span onClick={handleAuth}>Sign out</span>
+        </DropDown>
+        </SignOut>
+        </>)
+        }
     </Nav>
   )
 }
@@ -190,6 +198,37 @@ const Login = styled.a`
     }
 `
 
+const UserImg =styled.img`
+height: 100%;
+`
+const DropDown = styled.div`
+    position: absolute;
+    top: 48px;
+    right: 0px;
+    background: rgb(19, 19, 19);
+    border: 1px solid rgba(151, 151, 151, 0.34) ;
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 /50%) 0px 0px 18px 0px;
+    padding: 10px;
+    font-size: 14px;
+    letter-spacing: 3px;
+    width: 100px;
+    opacity: 0;
+`
+
+const SignOut = styled.div`
+    position: relative;
+    height: 48px;
+    width: 48px;
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+
+    ${UserImg}{
+        border-radius: 50%;
+    }
+`
 
 
 export default Header
