adbaa37546dd881c44f69e6d4daea3ee6224b2d2
Lokesh Babchode <108623364+loki7349@users.noreply.github.com>
2024-01-25 12:58:02 +0000

    added the react slick carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 import React from 'react'
+import ImgSlider from "./ImgSlider";
 
 const Home = () => {
     return (
         <Container>
-            <h2>Hello</h2>
+            <ImgSlider />
         </Container>
     ) 
 }
